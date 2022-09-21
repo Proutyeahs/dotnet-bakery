c70faf294bf34a8df4209ec80b62abd87bbbccfa
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-09-21 16:02:46 -0500

    finished routes

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -5,11 +5,28 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace DotnetBakery.Models
 {
+
+    public enum BreadType
+    {
+        Sourdough,           // 0
+        Rye,                 // 1 etc.. v
+        Pumpernickel,
+        French,
+        Tortilla,
+        HoneyWheat,
+        Pretzel,
+        Boule,
+    }
     public class Bread 
     {
         public int id {get; set;}
         public string name {get; set;}
         public string description {get; set;}
+
+        // need a bread type
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public BreadType type {get; set;}
+        
         public int count {get; set;}
 
         // relation to the baker
