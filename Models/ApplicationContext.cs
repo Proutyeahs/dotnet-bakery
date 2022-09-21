9c1d6680d996f9e5a335ff331cfd356a5db2a49a
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-09-21 11:58:24 -0500

    break

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -4,5 +4,8 @@ namespace DotnetBakery.Models
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
+
+        // reference our model classes
+        public DbSet<Baker> Bakers {get; set;}
     }
 }
